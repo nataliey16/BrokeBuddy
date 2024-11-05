7658a9d36ba602934e5d674f41df8a24b4c010c9
Natalie <natalie.yeung16@gmail.com>
2024-11-05 10:46:36 -0700

    Implement submit function to form, excluding trans type

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import {View, Text, TextInput, StyleSheet} from 'react-native';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import {TransactionType} from '../utils/utility';
 
 type CheckBoxPropType = {
   label: string;
+  transType: TransactionType;
 };
 
 function CheckBox(props: CheckBoxPropType): React.JSX.Element {
-  const {label} = props;
+  const {label, transType} = props;
   return (
     <View style={styles.checkboxView}>
       <BouncyCheckbox
+        isChecked={transType === TransactionType.Essential} // Replace 'SomeValue' with the appropriate value
         size={25}
         fillColor="red"
         unFillColor="#FFFFFF"
@@ -18,7 +21,7 @@ function CheckBox(props: CheckBoxPropType): React.JSX.Element {
         iconStyle={{borderColor: 'red'}}
         innerIconStyle={{borderWidth: 2}}
         // textStyle={{fontFamily: 'JosefinSans-Regular'}}
-        onPress={(isChecked: boolean) => {
+        onPress={(isChecked: string) => {
           console.log(isChecked);
         }}
       />
--- src/screens/AddTransaction.tsx
+++ src/screens/AddTransaction.tsx
@@ -2,16 +2,15 @@ import React, {useState} from 'react';
 import {View, Text, TextInput, Button, Pressable} from 'react-native';
 import {CommonStyles} from '../utils/CommonStyles';
 import CheckBox from '../components/CheckBox';
-import {TransactionType} from '../utils/utility';
+import {
+  addEditTransaction,
+  defaultTransactionEntry,
+  getNewID,
+  TransactionType,
+} from '../utils/utility';
 
 function AddTransaction(): React.JSX.Element {
-  const [newTransEntry, setNewTransEntry] = useState({
-    id: '',
-    title: '',
-    desc: '',
-    amount: 0,
-    type: TransactionType.Essential,
-  });
+  const [newTransEntry, setNewTransEntry] = useState(defaultTransactionEntry);
 
   const handleInputChange = (name: string, value: any) => {
     setNewTransEntry(prevEntry => ({
@@ -19,6 +18,17 @@ function AddTransaction(): React.JSX.Element {
       [name]: value,
     }));
     console.log(value);
+  };
+
+  const handleSubmit = () => {
+    //generate an id associated with trans entry
+    const transactionWithId = {...newTransEntry, id: getNewID()};
+    console.log(transactionWithId);
+
+    addEditTransaction(transactionWithId);
+
+    //reset the form
+    setNewTransEntry(defaultTransactionEntry);
   };
 
   return (
@@ -33,16 +43,18 @@ function AddTransaction(): React.JSX.Element {
         style={[CommonStyles.txtInput, {height: 100}]}
         placeholder="Add Description"
         value={newTransEntry.desc}
+        onChangeText={value => handleInputChange('desc', value)}
       />
       <TextInput
         style={CommonStyles.txtInput}
         placeholder="Amount in CAD"
         value={newTransEntry.amount.toString()}
+        onChangeText={value => handleInputChange('amount', parseFloat(value))}
       />
-      <CheckBox label="Essential" />
-      <CheckBox label="Leisure" />
-      <CheckBox label="Others" />
-      <Pressable style={CommonStyles.button}>
+      <CheckBox label="Essential" transType={TransactionType.Essential} />
+      <CheckBox label="Leisure" transType={TransactionType.Leisure} />
+      <CheckBox label="Others" transType={TransactionType.Others} />
+      <Pressable style={CommonStyles.button} onPress={handleSubmit}>
         <Text style={CommonStyles.btnTxt}>Submit</Text>
       </Pressable>
     </View>
